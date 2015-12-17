86cb4825e7a254c8a8b3aee09aa54fa7312edc77
seabrus <seabrus@gmail.com>
2015-12-17 17:26:14 +0200

    Link Django CompanyList with Angular home/detail pages

--- da/static/da/ang/js/da-ang.js
+++ da/static/da/ang/js/da-ang.js
@@ -1,11 +1,13 @@
 var app = angular.module('djangy', ['ngRoute', 'ngResource', 'ngCookies', 'ngSanitize']);
 
+
 app.config( ['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-/*
+/*    ===   Valuable snippets for general setting up of Angular  ===
+
 http://django-angular.readthedocs.org/en/latest/integration.html
 var my_app = angular.module('MyApp').config(function($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -53,13 +55,15 @@ http://blog.brunoscopelliti.com/deal-with-users-authentication-in-an-angularjs-w
 app.config(['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
   $routeProvider.when('/', { 
     templateUrl: '/static/da/ang/html/home.html', 
-    controller: 'HomeController', 
+    controller: 'HomeController',
+    controllerAs: 'ctrl',
     routeName: 'home',
   });
 
   $routeProvider.when('/details/:id', { 
     templateUrl: '/static/da/ang/html/details.html', 
     controller: 'DetailsController', 
+    controllerAs: 'ctrl',
     routeName: 'details',
   });
 
@@ -73,10 +77,10 @@ app.config(['$routeProvider', '$locationProvider', function( $routeProvider, $lo
 // =============================================================================
 //   Controllers
 // =============================================================================
-
 app.controller('MainController', [ '$location', function( $location ) {
 	var self = this;
 
+    // Class for the current menu item 
     self.getClass = function( path ) {
         if ($location.path().indexOf(path) !== -1) {
             return 'active';
@@ -88,8 +92,19 @@ app.controller('MainController', [ '$location', function( $location ) {
 
 app.controller('HomeController', [ 'CompanyListService', function( CompanyListService ) {
 	var self = this;
+    // "self.CompaniesData" is the main data object for this view
 
-	self.CompaniesData = CompanyListService.getListData();
+    CompanyListService.getListData()
+        .then( function(response) {  
+            self.CompaniesData = response.data;
+            return 1;
+        })
+        .catch( function() {
+            self.CompaniesData = [];
+            if ( window.console ) { 
+                console.log('getListData: Error when transferring data');
+            }
+        });
 }]);
 
 app.controller('DetailsController', [ '$routeParams', 'CompanyListService', function( $routeParams, CompanyListService ) {
@@ -104,8 +119,8 @@ app.controller('DetailsController', [ '$routeParams', 'CompanyListService', func
 // =============================================================================
 app.factory('CompanyListService', [ '$http', function( $http ) {
 
-	var dbListData = [         // Retain it as at least an empty [{}] 
-        {
+	var CompanyListData = [];
+/*        {
             id: 1,
             companyName: 'ABC',
             foundedAt: '1970',
@@ -115,52 +130,9 @@ app.factory('CompanyListService', [ '$http', function( $http ) {
             subscriptionPlan: 'Business plan',
             hours: [
               { dayName: 'Tuesday', from: '9:00', until: '12:30', db_id: 21 },
-              { dayName: 'Thursday', from: '14:00', until: '18:00', db_id: 45 },
-              { dayName: 'Friday', from: '14:00', until: '16:30', db_id: 55 },
-              { dayName: 'Wednesday', from: '', until: '', db_id: 30 },
-              { dayName: 'Sunday', from: '12:00', until: '13:30', db_id: 79 },
-              { dayName: 'Sunday', from: '19:00', until: '23:30', db_id: 77 },
             ],
     	},
-
-        {
-            id: 5,
-            companyName: 'Nokia',
-            foundedAt: '1982',
-            email: 'ccc@nokia.com',
-            logoUrl: '/media/da/logo2.jpg',
-            paymentMethod:  'Credit card',
-            subscriptionPlan: 'Business plan',
-            hours: [
-              { dayName: 'Friday', from: '14:00', until: '16:30', db_id: 55 },
-              { dayName: 'Saturday', from: '15', until: '20', db_id: 62 },
-              { dayName: 'Sunday', from: '12:00', until: '13:30', db_id: 79 },
-              { dayName: 'Sunday', from: '19:00', until: '23:30', db_id: 77 },
-            ],
-    	},
-
-        {
-            id: 12,
-            companyName: 'Colombo',
-            foundedAt: '2010',
-            email: '12@colombo.cc',
-            logoUrl: '/media/da/logo3.png',
-            paymentMethod: 'Bank transfer',
-            subscriptionPlan: 'Business plan',
-            hours: [
-              { dayName: 'Monday', from: '9:00', until: '12:30', db_id: 11 },
-              { dayName: 'Tuesday', from: '9:00', until: '12:30', db_id: 21 },
-              { dayName: 'Thursday', from: '14:00', until: '18:00', db_id: 45 },
-              { dayName: 'Monday', from: '14:00', until: '16:30', db_id: 12 },
-              { dayName: 'Friday', from: '14:00', until: '16:30', db_id: 55 },
-              { dayName: 'Monday', from: '19:00', until: '23:30', db_id: 15 },
-              { dayName: 'Tuesday', from: '19:00', until: '23:30', db_id: 22 },
-              { dayName: 'Wednesday', from: '', until: '', db_id: 30 },
-            ],
-    	},
-
-    ];
-
+*/
 	var CompanyDetailsData = {
         id: 0,
         companyName: '',
@@ -181,45 +153,89 @@ app.factory('CompanyListService', [ '$http', function( $http ) {
 	};
 
 
-    return  { 
-        getListData: function() { 
-            return dbListData; 
-        },
+    // Conform DB and client-side data structures and properties names
+    function prepareData(data) {
+        CompanyListData = [];
 
-        getItemData: function( id ) { 
-            if ( !id )  return {};
+        var dbProps  = [ "id", "company_name", "founded_at", "email", "payment_method", "subscription_plan", "logo_url", ];
+        var angProps = [ "id", "companyName", "foundedAt", "email", "paymentMethod", "subscriptionPlan", "logoUrl", ];
+        var dbHoursPr  = [ "id", "day_name", "from_time", "until_time", "db_id", ];
+        var angHoursPr = [ "id", 'dayName', 'from', 'until', 'db_id', ];
 
-            for ( var i=0, len=dbListData.length; i<len; i++ ) {
-                if (id === dbListData[i].id + '' )
-                    break;
-            }
-            if ( i === len )  return {};
+        for (var k=0, len=data.length; k<len; k++ ) {
+            var item = {};
 
-            var dbData = dbListData[i];
-
-            var commonParams = [ 'id', 'companyName', 'foundedAt', 'email', 'logoUrl', 'paymentMethod', 'subscriptionPlan' ];
-            for (var k=0, len=commonParams.length; k < len; k++) {
-                CompanyDetailsData[ commonParams[k] ] = dbData[ commonParams[k] ];
+            for (var n=0, len1=dbProps.length; n<len1; n++ ) {
+                item[ angProps[n] ] = data[ k ][ dbProps[n] ]; 
             }
 
-            var days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
-            for (var k=0; k < 7; k++) {
-                CompanyDetailsData.openingHours[ k ].hours = [];
-                for (var j=0, len=dbData.hours.length; j < len; j++) {
-                    if ( dbData.hours[ j ].dayName === days[k] ) {
-                        var times ={};
-                        times.from = dbData.hours[ j ].from;
-                        times.until = dbData.hours[ j ].until;
-                        times.db_id = dbData.hours[ j ].db_id;
-                        CompanyDetailsData.openingHours[ k ].hours.push( times );
-                    }
+            item['hours'] = [];
+            for (var m=0, len2=data[ k ]['hours'].length; m<len2; m++ ) {
+                var hourItem = {};
+
+                for (var p=0, len3=dbHoursPr.length; p<len3; p++ ) {
+                    hourItem[ angHoursPr[p] ] = data[ k ]['hours'][ m ][ dbHoursPr[p] ]; 
+                }
+                item['hours'].push( hourItem );
+            }
+
+            CompanyListData.push( item );
+        }
+
+        data = [];
+
+        return CompanyListData; 
+    }   // end of "function prepareData(data) ..."
+
+
+   // Prepare data structures for an individual company
+    function prepareDetailData( id ) {
+        if ( !id )  return {};
+
+        for ( var i=0, len=CompanyListData.length; i<len; i++ ) {
+            if (id === CompanyListData[i].id + '' )
+                break;
+        }
+        if ( i === len )  return {};
+
+        var dbData = CompanyListData[i];
+
+        var commonParams = [ 'id', 'companyName', 'foundedAt', 'email', 'logoUrl', 'paymentMethod', 'subscriptionPlan' ];
+        for (var k=0, len=commonParams.length; k < len; k++) {
+            CompanyDetailsData[ commonParams[k] ] = dbData[ commonParams[k] ];
+        }
+
+        var days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+        for (var k=0; k < 7; k++) {
+            CompanyDetailsData.openingHours[ k ].hours = [];
+            for (var j=0, len=dbData.hours.length; j < len; j++) {
+                if ( dbData.hours[ j ].dayName === days[k] ) {
+                    var times ={};
+                    times.from = dbData.hours[ j ].from;
+                    times.until = dbData.hours[ j ].until;
+                    times.db_id = dbData.hours[ j ].db_id;
+                    CompanyDetailsData.openingHours[ k ].hours.push( times );
                 }
             }
+        }
 
-            return CompanyDetailsData; 
-        }, 
- 
-    };
+        return CompanyDetailsData; 
+    }
+
+
+    return  { 
+        getListData: function() { 
+            url = '/companies/';
+            return $http({
+                method: 'GET',
+                url: url,
+                responseType: 'json',
+                transformResponse: prepareData, 
+            });
+        },
+
+        getItemData: prepareDetailData,
+     };
 
 }]);
 
